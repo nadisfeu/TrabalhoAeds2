851e86fedb59ce991957b182a30e52fdeb04f5cc
Géssica Bomfim <93146698+gessicatb@users.noreply.github.com>
2023-11-26 17:31:25 -0300

    Add files via upload
    organizando tudo

---
+++ Entidades/interface.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "interface.h"
+
+void MSG_MENU()
+{
+    system("cls");
+    printf("\n\n\t>>>>>>>>>>>>>>>>>>>>>>> OPCOES DE MENU <<<<<<<<<<<<<<<<<<<<<<<<");
+    printf("\n\n\t1. ADOCIONAR PEDIDOS A UMA MESA");
+    printf("  \n\t2. BUSCAR FUNCIONARIOS SEUQENCIAL (estao desordenados)");
+    //printf("  \n\t3. EXCLUIR");
+    printf("  \n\t3. EXIBIR INFORMACOES SOBRE A MESA");
+    printf("  \n\t4. SAIR");
+}
+
+void MENU(FILE *mesas, FILE *pedidos, FILE *funcionarios, FILE *log, int qntMesas)
+{
+    TMesa *mesa1;
+    TPedido *pedido;
+    TFunc *func;
+    int opcao = 0;
+    int aux;
+    do
+    {
+        MSG_MENU();
+        printf("\n\nDigite uma opcao: ");
+        fflush(stdin);
+        scanf("%d", &opcao);
+        switch (opcao)
+        {
+        case 1: // INSERIR PEDIDO EM MESA
+            printf("\nDIGITE O CODIGO MESA DESEJA ADICIONAR O PEDIDO\n");
+            fflush(stdin);
+            scanf("%d", &aux);
+            mesa1 = BuscaBinariaMesa(log, aux, mesas, (qntMesas - (qntMesas - 1)), qntMesas);
+
+            if (mesa1 != NULL)
+            {
+                imprimeTodosPedido(pedidos);
+                printf("Digite o codigo do pedido que deseja fazer:\n");
+                fflush(stdin);
+                scanf("%d", &aux);
+                fazPedidoMesa(log, mesas, pedidos, mesa1, aux);
+                printf("Pedido feito com sucesso!\n");
+            }
+            else
+            {
+                printf("Digite um codigo de mesa valido!");
+            }
+            system("PAUSE");
+            break;
+        case 2: // PESQUISAR
+             printf("\nInforme o codigo do funcionario que deseja pesquisar: ");
+             fflush(stdin);
+             scanf("%d", &aux);
+
+             func = BuscaSequencial(log,funcionarios,aux,qntMesas/3); 
+             if (func != NULL)
+                 imprime(func);
+             else
+                 printf("\nO funcionario NAO existe!\n\n");
+             system("PAUSE");
+             break;
+        /*case 3: // EXCLUIR
+             printf("\nInforme o codigo do pedido que deseja excluir: ");
+             scanf("%d", &pedido.cod);
+
+             Excluir(mesa1, &pedido);
+
+             if (pedido.cod != -1)
+                 printf("\nItem excluido com sucesso!\n");
+             else
+                 printf("\n >>>>>Nao foi possivel excluir o pedido! Pedido NAO pertence a mesa!!<<<< \n");
+
+             break;*/
+         case 3: // IMPRIMIR
+             printf("\n DIGITE O CODIGO DA MESA VOCE DESEJA OBTER AS INFORMACOES?\n");
+             fflush(stdin);
+             scanf("%d", &aux);
+             mesa1 = BuscaBinariaMesa(log, aux, mesas, (qntMesas - (qntMesas - 1)), qntMesas);
+             printf("\n\n\t >>>>>> IMPRIMINDO AS INFORMACOES DA MESA!! <<<<<<");
+
+             if (mesa1 != NULL)
+            {
+               imprimeMesa(*mesa1);
+            }
+            else
+            {
+                printf("Digite um codigo de mesa valido!");
+            }
+            system("PAUSE");
+
+             break;
+        case 4:
+            system("cls");
+            printf("\n\n\n\t >>>>>> MSG: Saindo do MODULO...!!! <<<<<<\n");
+            system("PAUSE");
+            break;
+        default:
+            // system("clear");
+            system("cls");
+            printf("\n\n\n\t >>>>>> MSG: Digite uma opcao valida!!! <<<<<<\n");
+            system("PAUSE");
+        } // fim do bloco switch
+    } while (opcao != 4);
+}
