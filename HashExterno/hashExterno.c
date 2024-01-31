479b82690a1b89cc58e1cf70460638c7bb952878
Fernando-nitro <fnetofkam@gmail.com>
2024-01-31 15:17:48 -0300

    avançando na inserção

--- HashExterno/hashExterno.c
+++ HashExterno/hashExterno.c
@@ -22,6 +22,7 @@ void adiciona_no_hash(TMesa *mesa, FILE *hashCompartimentos, FILE *hash, int tam
 
     aux = leCabecalho(hash);
 
+    //no caso da lista está vazia, apenas adiciona
     if (aux->tam == 0)
     {
         fseek(hashCompartimentos, aux->pos  , SEEK_SET);
@@ -30,7 +31,8 @@ void adiciona_no_hash(TMesa *mesa, FILE *hashCompartimentos, FILE *hash, int tam
         salvaLista(aux,hash);
         return;
     }
-    
+    //caso nao esteja, tratamento de colisoes e adiciona na posição ao lado
+
 }
 
 void criaHash(char *nome_do_hash, int tam, FILE *mesas)
@@ -56,9 +58,11 @@ void criaHash(char *nome_do_hash, int tam, FILE *mesas)
             lista.tam = 0;
             lista.pos = ftell(hashCompartimentos); //salva o endereço do primeiro elemento daquela lista
             lista.prox = i + 1;
+            for(int j = 0; j<10 ;j++){   //10 tá aqui temporariamente 
+                salvaMesa(&mesa, hashCompartimentos);
+            }
             salvaLista(&lista, hash);
         }
     }
 }
 
-// cria uma tabela preenchida com os códigos iguais a -1, depois faz os calculos.
